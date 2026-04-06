136c7d8fc07261692bfc5344e6d624b2b7e237c8
morrimouth <morrimouth@gmail.com>
2026-04-06 04:42:36 +0000

    Downgrade react-native-reanimated to 3.10.1 for compatibility

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,9 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
+    plugins: [
+      // react-native-reanimated/plugin must be listed last
+      'react-native-reanimated/plugin',
+    ],
   };
 };
