50af7194ac22be063167515a9db3915c20836751
LockSafe Dev <dev@locksafe.uk>
2026-04-27 13:31:50 +0000

    Increment iOS build to 19 for App Store submission

--- app.config.js
+++ app.config.js
@@ -83,7 +83,7 @@ module.exports = () => {
       ios: {
         supportsTablet: true,
         bundleIdentifier: getBundleId(),
-        buildNumber: '18',
+        buildNumber: '19',
         infoPlist: {
           NSLocationWhenInUseUsageDescription:
             'LockSafe needs your location to find nearby locksmiths and track job progress.',
